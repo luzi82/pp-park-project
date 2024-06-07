62dff42c5df4c40c03a8a25181528abfaec6e8ca
luzi82 <luzi82@gmail.com>
2024-06-07 16:36:07 +0800

    get server password by ini

--- ppp-patrol/common/pw.py
+++ ppp-patrol/common/pw.py
@@ -1,3 +1,13 @@
-import _config
+# import _config
+# print(_config.PALWORLD_PASSWORD)
 
-print(_config.PALWORLD_PASSWORD)
+import re
+
+txt = ''
+with open('/root/palworld-server/data/Config/LinuxServer/PalWorldSettings.ini') as fin:
+  for line in fin:
+    line = line.strip()
+    txt += line
+
+m = re.search('ServerPassword="([^?"]*)"',txt)
+print(m.group(1))
