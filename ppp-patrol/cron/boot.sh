a877c54a5eedb6e14db2378672b560fd634000f0
luzi82 <luzi82@gmail.com>
2024-06-07 16:38:14 +0800

    download config b4 start

--- ppp-patrol/cron/boot.sh
+++ ppp-patrol/cron/boot.sh
@@ -3,6 +3,13 @@
 MY_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PATROL_PATH=`/usr/bin/dirname ${MY_PATH}`
 
+aws s3 cp \
+  s3://luzi82-palworld/config/current/PalWorldSettings.ini \
+  /root/palworld-server/data/Config/LinuxServer/
+aws s3 cp \
+  s3://luzi82-palworld/config/current/WorldOption.sav \
+  /root/palworld-server/data/Config/LinuxServer/
+
 docker container restart palworld-server_palworld_1
 
 ${PATROL_PATH}/broadcast-ip/broadcast-ip.sh
